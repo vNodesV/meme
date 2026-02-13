7ca95d6019c2e421a3807413beb7d5479b82999e
vNodesV <dsebster@vnodes.cloud>
2026-02-13 04:53:49 -0500

    Refactor MakeEncodingConfig function to improve code readability by removing unnecessary blank lines

--- app/params/proto.go
+++ app/params/proto.go
@@ -26,17 +26,17 @@ const (
 // The InterfaceRegistry is initialized with address codecs to enable transaction signing.
 func MakeEncodingConfig() EncodingConfig {
 	amino := codec.NewLegacyAmino()
-	
+
 	// Get SDK config for Bech32 prefixes
 	cfg := sdk.GetConfig()
 	cfg.SetBech32PrefixForAccount(bech32PrefixAccAddr, bech32PrefixAccPub)
 	cfg.SetBech32PrefixForValidator(bech32PrefixValAddr, bech32PrefixValPub)
 	cfg.SetBech32PrefixForConsensusNode(bech32PrefixConsAddr, bech32PrefixConsPub)
-	
+
 	// Create address codecs
 	accCodec := addresscodec.NewBech32Codec(cfg.GetBech32AccountAddrPrefix())
 	valCodec := addresscodec.NewBech32Codec(cfg.GetBech32ValidatorAddrPrefix())
-	
+
 	// Create InterfaceRegistry with proper address codecs for transaction signing
 	interfaceRegistry, err := types.NewInterfaceRegistryWithOptions(types.InterfaceRegistryOptions{
 		ProtoFiles: proto.HybridResolver,
@@ -48,10 +48,10 @@ func MakeEncodingConfig() EncodingConfig {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Register ed25519 public key
 	interfaceRegistry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ed25519.PubKey{})
-	
+
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
 
